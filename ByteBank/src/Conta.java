64696e71609614e3dab513046cb9d4e8e1e592a2
lais.lopes <lais.lopes@setup.com.br>
2018-09-15 16:49:09 -0300

    Criado construtor da classe conta e criada a classe Pessoa.

--- ByteBank/src/Conta.java
+++ ByteBank/src/Conta.java
@@ -1,9 +1,21 @@
 
 public abstract class Conta {
     
-    private String numeroDaConta;
-    private String numeroAgencia;
-    private String titularDaConta;
-    private double saldoDaConta; 
+    private int numeroDaConta;
+    private int numeroAgencia;
+    private Pessoa titularDaConta;
+    private double saldoDaConta;
     
-}
+    
+    public Conta(int numeroDaConta){
+        
+        if(numeroDaConta > 0){
+            
+            this.numeroDaConta = numeroDaConta;
+        }
+        else{
+            System.out.println("O número da Conta não pode ser negativo!");
+        }
+    }
+    }
+         
