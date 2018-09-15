b83b04c0763f88fe1ef1fcc3d22915d108a6432d
lais.lopes <lais.lopes@setup.com.br>
2018-09-15 17:01:14 -0300

    Add atributos e métodos das classes PF e PJ.

--- ByteBank/src/PessoaFisica.java
+++ ByteBank/src/PessoaFisica.java
@@ -1,4 +1,21 @@
 
 public class PessoaFisica extends Pessoa {
     
+    String CPF;
+    
+    public PessoaFisica(String CPF){
+        
+        this.CPF = CPF;
+    }
+
+    public String getCPF() {
+        return this.CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+    
+    
+            
 }
--- ByteBank/src/PessoaJuridica.java
+++ ByteBank/src/PessoaJuridica.java
@@ -1,4 +1,20 @@
 
 public class PessoaJuridica extends Pessoa {
     
+    String CNPJ;
+    
+    public PessoaJuridica(String CNPJ){
+        
+        this.CNPJ = CNPJ;
+    }
+
+    public String getCNPJ() {
+        return this.CNPJ;
+    }
+
+    public void setCNPJ(String CNPJ) {
+        this.CNPJ = CNPJ;
+    }
+    
+    
 }
